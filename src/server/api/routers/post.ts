e2a123207abc39aee9a1cc605251a7aea413c37b
Vignesh Prasad <vigfb9295@gmail.com>
2024-03-15 00:44:06 +0530

    removed post from router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,19 +15,19 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+//   create: protectedProcedure
+//     .input(z.object({ name: z.string().min(1) }))
+//     .mutation(async ({ ctx, input }) => {
+//       // simulate a slow db call
+//       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
-      });
-    }),
+//       return ctx.db.post.create({
+//         data: {
+//           name: input.name,
+//           createdBy: { connect: { id: ctx.session.user.id } },
+//         },
+//       });
+//     }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
