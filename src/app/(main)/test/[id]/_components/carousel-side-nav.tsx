a87c18530c923cfb2c141ed2a9b1213ef9eea7ba
Vignesh Prasad <vigfb9295@gmail.com>
2024-04-05 01:25:37 +0530

    fixed clamp error

--- src/app/(main)/test/[id]/_components/carousel-side-nav.tsx
+++ src/app/(main)/test/[id]/_components/carousel-side-nav.tsx
@@ -18,10 +18,15 @@ export function CarouselSideNav({ disabled=false, onClick, isActive=false, id, t
       onClick={onClick}
       className={`bg-white mb-2 rounded-md min-h-32 border-2 relative ${isActive ? 'border-accent-1-200' : ''}`}
       key={id}>
+        <div className='text-sm p-4'>
+          <div className="line-clamp-2">
+            {title}
+          </div>
+        </div>
         <div className="absolute right-2 top-2">
           {
             status === QuestionStatus.CORRECT &&
-              <p className="text-md"> ✅ </p>
+            <p className="text-md"> ✅ </p>
           }
           {
             status === QuestionStatus.INCORRECT &&
@@ -35,10 +40,11 @@ export function CarouselSideNav({ disabled=false, onClick, isActive=false, id, t
             status === QuestionStatus.NEED_TO_CHECK &&
             <p className="text-md"> ❔ </p>
           }
+          {
+            status === QuestionStatus.ERROR &&
+            <p className="text-md"> ❗ </p>
+          }
         </div>
-        <span className='text-sm line-clamp-3 p-4'>
-          {title}
-        </span>
     </button>
   );
 }
