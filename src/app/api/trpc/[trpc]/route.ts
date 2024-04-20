a766007d81baba56d78306992611b59b2a6b36f2
Vignesh Prasad <vigfb9295@gmail.com>
2024-04-20 16:23:54 +0530

    testing timeouts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -5,9 +5,8 @@ import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-export const maxDuration = 30; // This function can run for a maximum of 5 seconds
+export const maxDuration = 60; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
-
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
